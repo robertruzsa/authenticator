f15f73153f10cbb8fd99bf06fb971f3325c8941b
robertruzsa <ruzarobert@gmail.com>
2022-01-15 22:48:43 +0100

    Add CodeReaderView with interoperability

--- app/src/main/java/com/robertruzsa/authenticator/ui/screens/otpform/OTPFormScreen.kt
+++ app/src/main/java/com/robertruzsa/authenticator/ui/screens/otpform/OTPFormScreen.kt
@@ -1,11 +1,23 @@
 package com.robertruzsa.authenticator.ui.screens.otpform
 
-import androidx.compose.material.Text
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import com.robertruzsa.authenticator.databinding.FragmentCodeReaderBinding
+import com.robertruzsa.codereaderview.BarcodeFormat
 
 @Preview
 @Composable
 fun NewItemScreen() {
-    Text(text = "new item")
+    AndroidViewBinding(
+        factory = FragmentCodeReaderBinding::inflate,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        codeReader.setOnBarcodeScannedListener(BarcodeFormat.ALL_FORMATS) {
+            Log.d("robi", it)
+        }
+    }
 }

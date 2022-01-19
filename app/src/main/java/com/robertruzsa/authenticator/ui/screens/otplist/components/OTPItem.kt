eebc1d14cb6bb5239e48591c35d825f54e61d70f
robertruzsa <ruzarobert@gmail.com>
2022-01-19 19:59:19 +0100

    Add state hoisting for CountDownIndicator

--- app/src/main/java/com/robertruzsa/authenticator/ui/screens/otplist/components/OTPItem.kt
+++ app/src/main/java/com/robertruzsa/authenticator/ui/screens/otplist/components/OTPItem.kt
@@ -2,16 +2,16 @@ package com.robertruzsa.authenticator.ui.screens.otplist.components
 
 import android.os.Handler
 import android.os.Looper
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.robertruzsa.authenticator.domain.model.OTPAccount
+import com.robertruzsa.ui.components.CountDownIndicator
 import dev.turingcomplete.kotlinonetimepassword.HmacOneTimePasswordGenerator
 import dev.turingcomplete.kotlinonetimepassword.TimeBasedOneTimePasswordGenerator
 import org.apache.commons.codec.binary.Base32
@@ -21,38 +21,9 @@ import org.apache.commons.codec.binary.Base32
 fun OTPItem(
     account: OTPAccount? = null
 ) {
-
-    var otpCode by remember {
-        mutableStateOf(generateOTPCode(account!!))
-    }
-
-    DisposableEffect(Unit) {
-        val handler = Handler(Looper.getMainLooper())
-
-        val runnable = object : Runnable {
-            override fun run() {
-                otpCode = generateOTPCode(account!!)
-                handler.postDelayed(this, 5_000)
-            }
-        }
-
-        handler.post(runnable)
-
-        onDispose {
-            handler.removeCallbacks(runnable)
-        }
-    }
-
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        elevation = 8.dp
-    ) {
-        Column(
-            modifier = Modifier.padding(8.dp),
-        ) {
-            Text(text = account?.accountName ?: "")
-            Text(text = otpCode)
-        }
+    when (account) {
+        is OTPAccount.TOTPAccount -> TOTPItem(account)
+        is OTPAccount.HOTPAccount -> HOTPItem(account)
     }
 }
 
@@ -73,4 +44,61 @@ private fun generateOTPCode(otpAccount: OTPAccount): String {
             ).generate(otpAccount.counter)
         }
     }
+}
+
+@Composable
+fun TOTPItem(account: OTPAccount.TOTPAccount) {
+    var otpCode by remember {
+        mutableStateOf(generateOTPCode(account))
+    }
+
+    var progress by remember {
+        mutableStateOf(0f)
+    }
+
+    val periodMillis: Long = account.config.timeStep * 1000
+
+    DisposableEffect(Unit) {
+        val handler = Handler(Looper.getMainLooper())
+
+        val runnable = object : Runnable {
+            override fun run() {
+                otpCode = generateOTPCode(account)
+                progress = 0f
+                handler.postDelayed(this, periodMillis)
+            }
+        }
+
+        handler.post(runnable)
+
+        onDispose {
+            handler.removeCallbacks(runnable)
+        }
+    }
+
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = 8.dp
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+        ) {
+            Text(text = account.accountName)
+            Text(text = otpCode)
+            CountDownIndicator(
+                currentProgress = progress,
+                onTick = { progressUnit ->
+                    progress += progressUnit
+                },
+                countDownFromMillis = periodMillis,
+                modifier = Modifier.size(20.dp),
+                color = Color.Blue
+            )
+        }
+    }
+}
+
+@Composable
+fun HOTPItem(account: OTPAccount.HOTPAccount) {
+    // TODO
 }
---
+++ ui/src/main/java/com/robertruzsa/ui/components/CountDownIndicator.kt
@@ -0,0 +1,59 @@
+package com.robertruzsa.ui.components
+
+import android.os.Handler
+import android.os.Looper
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun CountDownIndicator(
+    currentProgress: Float,
+    onTick: (Float) -> Unit,
+    countDownFromMillis: Long,
+    modifier: Modifier = Modifier,
+    color: Color
+) {
+
+    val sectorUnitSize: Float =
+        FULL_ROTATION_DEGREES / (countDownFromMillis / REDRAW_INTERVAL_MILLIS)
+
+    DisposableEffect(Unit) {
+        val handler = Handler(Looper.getMainLooper())
+
+        val runnable = object : Runnable {
+            override fun run() {
+                onTick(sectorUnitSize)
+                handler.postDelayed(this, REDRAW_INTERVAL_MILLIS)
+            }
+        }
+
+        handler.post(runnable)
+
+        onDispose {
+            handler.removeCallbacks(runnable)
+        }
+    }
+
+    Box(
+        modifier = modifier
+    ) {
+        Canvas(
+            modifier = modifier
+        ) {
+            drawArc(
+                color = color,
+                startAngle = START_ANGLE,
+                sweepAngle = currentProgress,
+                useCenter = true,
+            )
+        }
+    }
+}
+
+const val FULL_ROTATION_DEGREES: Float = 360f
+const val START_ANGLE: Float = -90f
+const val REDRAW_INTERVAL_MILLIS: Long = 100
